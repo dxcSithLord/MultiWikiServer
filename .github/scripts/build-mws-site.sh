11cb30a05748dbe69aab9831995cb08112c466b1
arlen22 <arlen22github@gmail.com>
2025-04-07 17:07:47 -0400

    setup mws-docs build script

--- .github/scripts/build-mws-site.sh
+++ .github/scripts/build-mws-site.sh
@@ -94,6 +94,7 @@ git config --global user.email "actions@github.com"
 git config --global user.name "GitHub Actions"
 git add -A .
 git commit --message "GitHub build: $GITHUB_RUN_NUMBER of $TW5_BUILD_BRANCH ($(date +'%F %T %Z'))"
+git show -q
 git remote add deploy "https://$GH_TOKEN@github.com/TiddlyWiki/mws.tiddlywiki.com-gh-pages.git" &>/dev/null
 git push deploy main &>/dev/null
 popd
