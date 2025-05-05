f25f05068e098bffaf033ae06128b0d582e83147
arlen22 <arlen22github@gmail.com>
2025-05-05 11:58:10 -0400

    serve docs wiki from MWS directly (node data folder style), proof of concept

--- src/routes/tw-test.ts
+++ src/routes/tw-test.ts
@@ -1,22 +1,24 @@
-import { Server } from "http";
-import { rootRoute } from "./router";
+import { rootRoute, SiteConfig } from "./router";
 import { ZodAssert as zodAssert } from "../utils";
-import { Tiddler, TiddlyWiki, TW } from "tiddlywiki";
+import { TiddlyWiki, TW } from "tiddlywiki";
+import { STREAM_ENDED } from "../streamer";
 
-export function TWRoutes(parent: rootRoute) {
-  const wikiRouter = new TWRouter();
+export function DocsRoute(parent: rootRoute, config: SiteConfig) {
+  const wikiRouter = new TWRouter(config);
   parent.defineRoute({
-    method: ["OPTIONS", "GET", "HEAD", "POST", "PUT"],
+    method: ["OPTIONS", "GET", "HEAD", "POST", "PUT", "DELETE"],
     bodyFormat: "stream",
     path: /^\/mws(\/|$)/,
-    pathParams: ["final_slash"]
+    pathParams: []
   }, async (state) => {
-    zodAssert.pathParams(state, z => ({ final_slash: z.string().optional() }));
-    if (!state.pathParams.final_slash) throw state.redirect(state.urlInfo.pathname + "/");
-    // literally don't do this
-    const { req, res } = (state as any).streamer;
+    if (state.urlInfo.pathname === "/mws") throw state.redirect("/mws/");
+    // @ts-expect-error because streamer is private
+    const { req, res } = state.streamer;
+    // regardless, this has to end with a slash
+    if (req.url === "/mws") req.url = "/mws/";
     // options not required
     wikiRouter.twserver.requestHandler(req, res, {} as any);
+    return new Promise((r, c) => res.on("finish", r).on("error", c));
   });
 }
 
@@ -24,41 +26,32 @@ class TWRouter {
   $tw: TW;
   twserver!: any;
 
-  constructor() {
+  constructor(private config: SiteConfig) {
     this.$tw = TiddlyWiki();
     // tiddlywiki [+<pluginname> | ++<pluginpath>] [<wikipath>] ...[--command ...args]
     this.$tw.boot.argv = [
-      // "++plugins/client",
-      // "++plugins/server",
-      // "+tiddlywiki/filesystem",
-      // "+tiddlywiki/tiddlyweb",
-      // "./editions/mws-server",
-      // "--mws-load-plugin-bags",
-      // "--build", "load-mws-demo-data",
-      // "--mws-listen", "port=5001", "host=::"
+      "+plugins/tiddlywiki/tiddlyweb",
+      "+plugins/tiddlywiki/filesystem",
+      "./editions/mws-docs",
     ];
+
+    this.$tw.preloadTiddler({
+      text: "$protocol$//$host$" + this.config.pathPrefix + "/mws/",
+      title: "$:/config/tiddlyweb/host",
+    });
+    // Boot the TW5 app
     this.$tw.boot.boot(() => {
-
-
-      this.$tw.preloadTiddler({
-        text: "$protocol$//$host$" + "/mws/",
-        title: "$:/config/tiddlyweb/host",
+      const Server = this.$tw.modules.execute("$:/core/modules/server/server.js", "tw-test.ts").Server;
+      this.twserver = new Server({
+        wiki: this.$tw.wiki,
+        variables: {
+          // do not use a trailing slash
+          "path-prefix": this.config.pathPrefix + "/mws",
+          "root-tiddler": "$:/core/save/all",
+        },
+        routes: [],
       });
-      // Boot the TW5 app
-      this.$tw.boot.boot(() => {
-        const Server = this.$tw.modules.execute("$:/core/modules/server/server.js", "tw-test.ts").Server;
-        this.twserver = new Server({
-          wiki: this.$tw.wiki,
-          variables: {
-            // do not use a trailing slash
-            "path-prefix": "/mws",
-            "root-tiddler": "$:/core/save/all",
-          },
-          routes: [],
-        });
-        this.$tw.hooks.invokeHook("th-server-command-post-start", this.twserver, null, "mws");
-      });
-
+      this.$tw.hooks.invokeHook("th-server-command-post-start", this.twserver, null, "mws");
     });
   }
 }
