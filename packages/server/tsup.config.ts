f0f43d0044ac22f000c5646c891eb8a311398002
arlen22 <arlen22github@gmail.com>
2025-06-09 18:15:52 -0400

    make sure build works

--- packages/server/tsup.config.ts
+++ packages/server/tsup.config.ts
@@ -18,10 +18,27 @@ export default defineConfig({
   minify: false,       // Set to true if you want minification
   splitting: false,     // Code splitting only if we need to lazy import
   cjsInterop: true,
+  /*
+    Enabling this option will fill in some code when building esm/cjs to make it work, 
+    such as __dirname which is only available in the cjs module and import.meta.url 
+    which is only available in the esm module
+
+    - When building the cjs bundle, it will compile `import.meta.url` as 
+      ```
+      typeof document === "undefined" 
+        ? new URL("file:" + __filename).href 
+        : document.currentScript && document.currentScript.src || new URL("main.js", document.baseURI).href
+      ```
+    - When building the esm bundle, it will compile `__dirname` as 
+      ```
+      path.dirname(fileURLToPath(import.meta.url))
+      ```
+   */
   shims: true,        // Add shims for Node.js built-in modules
   metafile: true,
+
   banner: (ctx) => ctx.format === "esm" ? {
-    js: `import {createRequire as __createRequire} from 'module'; const require=__createRequire(import.meta.url);import 'source-map-support/register.js';`,
+    js: `import 'source-map-support/register.js';`,
   } : {
     js: `require('source-map-support/register');`,
   },
