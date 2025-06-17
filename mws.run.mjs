ae8891ed7ec064b227c26d013bf2b6e11129f706
arlen22 <arlen22github@gmail.com>
2025-06-17 18:16:43 -0400

    mws.run.mjs

--- mws.run.mjs
+++ mws.run.mjs
@@ -13,19 +13,13 @@ if(pkg?.name === "@tiddlywiki/mws" && existsSync(`${cwd}/mws.dev.mjs`)) {
   process.exit(1);
 }
 
-if(!pkg?.dependencies?.["@tiddlywiki/mws"]) {
+if(!pkg?.dependencies?.["@tiddlywiki/mws"] || pkg.name !== "@tiddlywiki/mws-instance" || !pkg.private) {
   console.error("This does not appear to be a valid MWS data folder.");
   console.error("Please run this command in the root of your MWS data folder.");
   process.exit(1);
 }
 
-if(pkg.name !== "@tiddlywiki/mws-instance" || !pkg.private) {
-  // this might be a previous version or some other project using mws as a dependency
-  console.error("This appears to be an old version of MWS or some kind of custom project.");
-  process.exit(1);
-}
-
-import(`${cwd}/node_modules/@tiddlywiki/mws`).then(mws => mws.default()).catch(console.error);
+import(`${cwd}/node_modules/@tiddlywiki/mws/dist/mws.js`).then(mws => mws.default()).catch(console.error);
 
 function tryParseJSON(file) {
   try {
